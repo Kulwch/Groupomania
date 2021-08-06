6c8648b5b94321def7bbd1f035a2cc9556dcb8c3
Kulwch <76691359+Kulwch@users.noreply.github.com>
2021-08-06 17:40:47 +0200

    Set nodemon server to run on port 3000. Installed Sequelize and set app.js global settings.

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const sequelize = require('./utils/database')
+const User = require('./models/User');
+const userRoutes = require('./routes/user');
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+sequelize.sync() 
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,55 @@
+/**
+ * server settings
+ * 
+ * Defines construction of the server, and its parameters.
+ * 
+ * Default port used is 3000 yet if problem, terminal will indicate which other port would be used.
+ */
+
+const http = require('http');
+const app = require('./app');
+
+const normalizePort = val => {
+  const appPort = parseInt(val, 10);
+
+  if (isNaN(appPort)) {
+    return val;
+  }
+  if (appPort >= 0) {
+    return appPort;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
