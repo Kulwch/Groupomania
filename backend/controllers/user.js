9e536a48fe80bad616b8139cf590d5594000d71a
Kulwch <76691359+Kulwch@users.noreply.github.com>
2021-08-06 18:10:35 +0200

    Signup and login features work. Next step is implementing the auth middleware.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,5 +20,26 @@ exports.signup = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-
-}
+    User.findOne({email: req.body.email}).then(user => {
+        if(!user) {
+            return res.status(401).json({message: 'Utilisateur non trouvé '});
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid){
+                    return res.status(401).json({message: 'Mot de passe incorrect '
+        });
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: jwt.sign(
+                        { userId: user._id },
+                        process.env.TOKEN_KEY.toString(),
+                        { expiresIn: '24h' }
+                    )
+                });                
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
