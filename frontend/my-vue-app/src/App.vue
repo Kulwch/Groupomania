7639363757252534c2ac9f56ee4a1b204047996e
Kulwch <76691359+Kulwch@users.noreply.github.com>
2021-08-25 15:49:47 +0200

    Starting to set frontend structure with Vue.js and Vite.

---
+++ frontend/my-vue-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Groupomania logo" src="./assets/logo.png" />
+  <test msg="Groupomania" />
+</template>
+
+<script setup>
+import test from './components/test.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/my-vue-app/src/components/test.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Lien 1
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Lien 2</a>
+  </p>
+
+
+  <p>
+    Edit
+    <code>components/test.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
